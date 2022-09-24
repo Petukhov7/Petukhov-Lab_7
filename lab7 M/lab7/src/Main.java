d20fb1dcaace9066b3b23d88fb98f2143a62c26e
Petukhov7 <113873995+Petukhov7@users.noreply.github.com>
2022-09-24 12:49:21 +0300

    Add files via upload

---
+++ lab7 M/lab7/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Main {
+
+    public static final int cars_count = 6;
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(cars_count);
+        CountDownLatch countDownLatch = new CountDownLatch(cars_count);
+        CountDownLatch countDownLatchForFinish = new CountDownLatch(cars_count);
+        Car.setCountDownLatch(countDownLatch);
+        Car.setCountDownLatchForFinish(countDownLatchForFinish);
+        Car.setCyclicBarrier(cyclicBarrier);
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60, "1"), new Tunnel(2), new Road(40, "2"));
+        Car[] cars = new Car[cars_count];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        countDownLatch.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        countDownLatchForFinish.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+        Car.checkWinners();
+    }
+}
